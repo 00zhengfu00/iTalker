d1065a613d28c010573bdd3fee64b727eda7fac9
gg199402 <22830154@qq.com>
2017-11-05 15:45:49 +0800

    Client-提交4.2.0资源文件的创建

--- App/ITalker/app/src/main/java/com/example/ggxiaozhi/italker/MainActivity.java
+++ App/ITalker/app/src/main/java/com/example/ggxiaozhi/italker/MainActivity.java
@@ -1,41 +1,15 @@
 package com.example.ggxiaozhi.italker;
 
-import android.widget.EditText;
-import android.widget.TextView;
-
 import com.example.ggxiaozhi.common.app.Activity;
 
-import butterknife.BindView;
-import butterknife.OnClick;
 
-public class MainActivity extends Activity implements IView {
+public class MainActivity extends Activity {
 
-    @BindView(R.id.txt_result)
-    TextView mTextView;
-
-    @BindView(R.id.edit_query)
-    EditText mEditText;
-
-    private IPresenter mPresenter;
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
-    @OnClick(R.id.btn_submit)
-    void submit() {
-        mPresenter = new Presenter(this);
-        mPresenter.search();
-    }
 
-    @Override
-    public String getInputString() {
-        return mEditText.getText().toString();
-    }
-
-    @Override
-    public void setResultString(String result) {
-        mTextView.setText(result);
-    }
 }
